09b06854c5fe6f46a569e2530d3202c2b7680cbd
tu.hoang <hoangleanhtu@gmail.com>
2022-03-26 18:25:09 +0700

    init project

---
+++ src/test/java/bkit/solutions/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package bkit.solutions.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
