adaaadad5d1107b1550728f3c1321788e6a982b4
tu.hoang <hoangleanhtu@gmail.com>
2022-05-30 12:35:12 +0700

    Add Testcontainer RabbitMQ

--- src/test/java/bkit/solutions/springbootstudy/BaseApplicationIntegrationTests.java
+++ src/test/java/bkit/solutions/springbootstudy/BaseApplicationIntegrationTests.java
@@ -2,9 +2,14 @@ package bkit.solutions.springbootstudy;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 public abstract class BaseApplicationIntegrationTests {
 
+  @Container
+  private static BkitRabbitMqContainer bkitRabbitMqContainer = BkitRabbitMqContainer.getInstance();
 }
