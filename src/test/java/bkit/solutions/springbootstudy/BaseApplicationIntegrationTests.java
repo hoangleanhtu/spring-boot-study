b7486a326441117d20e4693ca3ffbc7a232d83c2
tu.hoang <hoangleanhtu@gmail.com>
2022-05-30 17:14:49 +0700

    Wait RabbitMQ started in integration tests

--- src/test/java/bkit/solutions/springbootstudy/BaseApplicationIntegrationTests.java
+++ src/test/java/bkit/solutions/springbootstudy/BaseApplicationIntegrationTests.java
@@ -8,27 +8,30 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.TestPropertySourceUtils;
-import org.testcontainers.containers.RabbitMQContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 @ActiveProfiles({"test"})
 @ContextConfiguration(initializers = Initializer.class)
 public abstract class BaseApplicationIntegrationTests {
 
-  @Container
-  private static RabbitMQContainer rabbit = new RabbitMQContainer(
-      "rabbitmq:3.10.2-management");
+  private static final GenericContainer rabbit = new GenericContainer(
+      "rabbitmq:3.10.2")
+      .withExposedPorts(5672)
+      .waitingFor(Wait.forListeningPort());
+
+  static {
+    rabbit.start();
+  }
 
   public static class Initializer implements
       ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     @Override
     public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-      TestPropertySourceUtils.addInlinedPropertiesToEnvironment(configurableApplicationContext, "spring.rabbitmq.port=" + rabbit.getAmqpPort());
+      TestPropertySourceUtils.addInlinedPropertiesToEnvironment(configurableApplicationContext, "spring.rabbitmq.port=" + rabbit.getMappedPort(5672));
       TestPropertySourceUtils.addInlinedPropertiesToEnvironment(configurableApplicationContext, "spring.rabbitmq.host=" + rabbit.getHost());
     }
   }
