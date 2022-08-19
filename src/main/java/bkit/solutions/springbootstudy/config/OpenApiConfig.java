41579d3d3ed1969e9d2755d9d4180de97e06d727
tu.hoang <hoangleanhtu@gmail.com>
2022-08-19 14:26:50 +0700

    Add security to Swagger

---
+++ src/main/java/bkit/solutions/springbootstudy/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package bkit.solutions.springbootstudy.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .components(new Components()
+            .addSecuritySchemes("auth-token",
+                new SecurityScheme().type(Type.APIKEY).in(In.HEADER).name("Authorization")));
+  }
+}
--- src/main/java/bkit/solutions/springbootstudy/controllers/SecuredController.java
+++ src/main/java/bkit/solutions/springbootstudy/controllers/SecuredController.java
@@ -1,5 +1,7 @@
 package bkit.solutions.springbootstudy.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecuredController {
 
   @GetMapping("/hr")
+  @Operation(security = { @SecurityRequirement(name = "auth-token") })
   public Map<String, String> hr() {
     return Map.of("room", "HR");
   }
 
   @GetMapping("/lab")
+  @Operation(security = { @SecurityRequirement(name = "auth-token") })
   public Map<String, String> lab() {
     return Map.of("room", "Lab");
   }
 
   @GetMapping("/canteen")
+  @Operation(security = { @SecurityRequirement(name = "auth-token") })
   public Map<String, String> canteen() {
     return Map.of("room", "Canteen");
   }
